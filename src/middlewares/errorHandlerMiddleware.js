16e78eede47277ed5903f846792cfb3d0e99968d
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-07-24 01:58:55 +0300

    created middlewares

---
+++ src/middlewares/errorHandlerMiddleware.js
@@ -0,0 +1,30 @@
+import { isHttpError } from 'http-errors';
+import { MongooseError } from 'mongoose';
+
+export const errorHandlerMiddleware = (error, req, res, next) => {
+  if (isHttpError(error)) {
+    return res.status(error.status).json({
+      status: error.status,
+      message: error.message,
+      errors: error.errors || []
+    });
+  }
+
+  if (error instanceof MongooseError) {
+    return res.status(500).json({
+      status: 500,
+      message: 'Mongoose error',
+      data: {
+        message: error.message,
+      },
+    });
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Internal server error',
+    data: {
+      message: error.message,
+    },
+  });
+};
