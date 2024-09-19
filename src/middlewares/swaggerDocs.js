501ead7fb6690ec8e2dde0c676c2ba7f41d02952
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-09-19 23:45:23 +0300

    corrected middleware

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -4,6 +4,8 @@ import fs from 'node:fs';
 
 import { SWAGGER_PATH } from '../constants/index.js';
 
+
+
 export const swaggerDocs = () => {
   try {
     const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
