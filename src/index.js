7e886f52f753d5e1a16edbe61b1b177ea232e8d0
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-09-19 23:46:37 +0300

    corrected the others

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { startServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
+
+
 (async () => {
   await initMongoConnection();
   startServer();
